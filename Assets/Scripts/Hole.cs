e594f0e96bec072a2f088c1564f369e92ddfb8f7
aramakis <shinji.aramaki@gmail.com>
2015-11-28 21:27:43 +0900

    first commit

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+
+	// どのボールを吸い寄せるかタグで指定
+	public string activeTag;
+
+
+	void OnTriggerStay (Collider other)
+	{
+		// コライダーに触れているオブジェクトのRigidbodyコンポーネントを取得
+		Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+		//ボールがどの方向にあるかを計算
+		Vector3 direction = transform.position - other.gameObject.transform.position;
+		direction.Normalize();
+
+		//タグに応じてポールに力を加える
+		if (other.gameObject.tag == activeTag)
+		{
+			// 中心地点でボールを止めるため速度を減速させる
+			r.velocity *= 0.9f;
+
+			r.AddForce(direction * r.mass * 20.0f);
+		}
+		else
+		{
+			r.AddForce(-direction * r.mass * 80.0f);
+		}
+	}
+
+}
